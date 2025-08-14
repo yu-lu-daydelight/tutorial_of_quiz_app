160cb8e79647d7638b1ed0bd4a1e00c545c331d4
yu_lu <yu.lu@daydelight.com>
2025-08-14 17:15:02 +0900

    Align two buttons vertically

--- lib/title/title_page.dart
+++ lib/title/title_page.dart
@@ -10,35 +10,48 @@ class TitlePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(
-                'Let\'s do some Quiz!',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue, // Adding color
+              Expanded(
+                flex: 1,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      'Let\'s do some Quiz!',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue, // Adding color
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('スタート'),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('スタート'),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('結果一覧'),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('結果一覧'),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-              SizedBox(height: 16),
             ],
           ),
         ),
